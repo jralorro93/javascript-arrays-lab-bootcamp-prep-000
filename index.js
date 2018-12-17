2d54a71184f29dcc2c6bb9f738f3b7af22aa50d8
Juan Alorro <jralorro93@gmail.com>
2018-12-17 22:40:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function destructivelyRemoveFirstKitten(name) {
 }
 
 function appendKitten(name){
-  var newName = ("Broom")
-  console.log(kittens.concat(newName);
+  var newName = ("Broom");
+  console.log(kittens.concat(newName));
   return name;
 }
